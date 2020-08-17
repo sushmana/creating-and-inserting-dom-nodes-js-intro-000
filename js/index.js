1ab62e18dc011c8b96337a834f9bbae026f054fc
SUSHMANA GUPTA <sushmanag73@gmail.com>
2020-08-17 18:57:00 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,12 +1,7 @@
-//this file is optional
-//you can use this to paste in js code and have it run when index.html is loaded
 var element = document.createElement('div');
-
 element.innerHTML = 'Hello, DOM!';
 element.style.backgroundColor = '#f9f9f9';
-
 document.body.appendChild(element);
-
 element.style.textAlign = 'center';
 
 var ul = document.createElement('ul');
@@ -16,10 +11,9 @@ for (let i = 0; i < 3; i++) {
   li.innerHTML = (i + 1).toString();
   ul.appendChild(li);
 }
-element.appendChild(ul);
 
+element.appendChild(ul);
 ul.style.textAlign = 'left';
 
 ul.removeChild(ul.querySelector('li:nth-child(2)'));
-
-ul.remove(); 
+ul.remove();
